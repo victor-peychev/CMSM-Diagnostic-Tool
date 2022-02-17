ff4083c50fe9f43c65df9833dd933da812881eec
victor-peychev <victor.peychev@gmail.com>
2022-02-17 22:25:45 +0000

    Sidebar added

--- server.py
+++ server.py
@@ -5,8 +5,13 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    return "Hello World"
+    return render_template("home.html")
+
+@app.route('/base')
+
+def base():
+    return render_template("base.html")
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template(404.html)
+    return render_template("404.html")
