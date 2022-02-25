d5a1bd85094930cb5d7ebfade880e6e82cd4c027
victor-peychev <victor.peychev@gmail.com>
2022-02-25 12:44:24 +0000

    Form Updated

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField
+from wtforms import StringField, SubmitField, SelectField, SelectMultipleField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -32,21 +32,27 @@ def page_not_found(e):
 
 # Form class
 class Form(FlaskForm):
-    test = StringField("Test", validators=[DataRequired()])
-    test1 = SelectField("Test1", choices=['Yes', 'No'], validators=[DataRequired()])
+    phantom_scratching = SelectField("Phantom Sctratching", choices=['None', 'Phantom Scratching', 'Vocalization during phantom scratching'], validators=[DataRequired()])
+    scratching_behind_head_or_ears = SelectField("Scratching and / or rubbing head or ears ", choices=['No', 'Yes'], validators=[DataRequired()])
+    vocalization = SelectMultipleField("Vocalization", choices=[('1', 'Spontaneous yelping  or when changing position when recumbent '), ('2','Postural'), ('3', 'Defecation'), ('4', 'Any vocalization')])
+    spinal_pain = SelectMultipleField("Spinal Pain", choices=['None', 'Cervical', 'Thoracolumar', 'Lumbosarcal', 'Any spinal pain'])
+    activity = SelectField("Activity", choices=['None', 'Reduced exercise', 'Lethargy', 'Both (lethargic and reduced exercise)', 'Any activity change'])
+    stairs_jumping = SelectField("Stairs/ Jumping", choices=['No', 'Yes'], validators=[DataRequired()])
+    behavior_change = SelectMultipleField("Behavior Change", choices=['Greeting', 'Aggression', 'Timid / Anxious', 'Wtihdrawn', 'Any behavior change'])
+    sleep = SelectField("Sleep disruption", choices=['No', 'Yes'], validators=[DataRequired()])
+    other_pain_behaviors = SelectMultipleField("Other Pain Behaviors", choices=['Touch / grooming  aversion ears / head and/or neck', 'Touch / grooming aversion 1-2  limb and /paw ', 'touch / grooming aversion sternum or flank', 'Abnormal awake Head / neck posture', 'Sleeping elevated or unusual head posture', 'Squinting / Avoiding light', 'Licking limb /   paw', 'Pain face', '1 or more pain behaviors / signs' ])
+    possibly_unrelated_behavior = SelectMultipleField("Possibly Unrelated Behavior", choices=['Repetitive tongue licking', 'Repetitive barking'])
+    neurological_abnormalities = SelectMultipleField("Neurological Abnormalities", choices=['Weakness', 'Muscle atrophy', 'Postural responses decreased', 'Ataxia', 'Hypermetria', 'Scoliosis / cervicothoracic torticollis', 'Any neurological abnormality'])
     submit = SubmitField("Submit")
 
 
 @app.route('/form', methods=['GET', 'POST'])
 def form():
-    test = None
-    test1 = None
+    vocalization = ''
     form = Form()
     if form.validate_on_submit():
-        test = form.test.data
-        form.test.data = ''
+        phantom_scratching = form.phantom_scratching.data
+        form.phantom_scratching.data = ''
     return render_template("form.html",
-    test = test,
-    test1= test1,
     form = form )
 
